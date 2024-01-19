44ff034243e9ac05c30875bb57f830103f949905
youta_ohshima <88968520+CreareWorks@users.noreply.github.com>
2024-01-20 01:42:04 +0900

    Update gpt.py

--- src/routers/gpt.py
+++ src/routers/gpt.py
@@ -15,31 +15,31 @@ async def merge_chunk_file_text(request: Request):
     return gpt_service.transcribe_speech(audio_data)
 
 
-# 一旦使わない プロンプト生成
-#@router.post("/gpt/transacription")
-#async def create_transacription(request: Request):
-#    try:
-#        transacript = await request.body()
-#        #文字起こし結果と定義済みのプロンプトから議事録を生成
-#        result = gpt_service.generate_minutes(transacript)
-#        
-#        return {
-#            "status_code": BASE_API_RESPONSE['REQUEST_SUCCESS'],
-#            "result": result
-#        }
-#        
-#    except Exception as e:
-#        raise HTTPException(
-#            status_code=BASE_API_RESPONSE['BAD_REQUEST'],
-#            detail=str(e)
-#        )
+# プロンプト生成
+@router.post("/gpt/transacription")
+async def create_transacription(request: Request):
+    try:
+        transacript = await request.body()
+        #文字起こし結果と定義済みのプロンプトから議事録を生成
+        result = gpt_service.generate_minutes(transacript)
+        
+        return {
+            "status_code": BASE_API_RESPONSE['REQUEST_SUCCESS'],
+            "result": result
+        }
+        
+    except Exception as e:
+        raise HTTPException(
+            status_code=BASE_API_RESPONSE['BAD_REQUEST'],
+            detail=str(e)
+        )
         
     # 呼び出しサンプル
     # 統合した文字列をプロンプト共にchatGPTにRequest
-    # url = "http://127.0.0.1:8000/gpt/transacription"
-    # headers = {"Content-Type": "text/plain"}
-    # async with httpx.AsyncClient() as client:
-    #     result = await client.post(url, headers=headers, data=concat_text) #ここに結合した文字列を投げる
+     url = "http://127.0.0.1:8000/gpt/transacription"
+     headers = {"Content-Type": "text/plain"}
+     async with httpx.AsyncClient() as client:
+         result = await client.post(url, headers=headers, data=concat_text) #ここに結合した文字列を投げる
 
 
 
